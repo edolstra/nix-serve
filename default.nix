bafcd0158668de6ef61a654a2a8f80fc816369ca
Eelco Dolstra <edolstra@gmail.com>
2020-11-24 11:21:48 +0100

    Turn into a flake

--- default.nix
+++ default.nix
@@ -1,24 +1,3 @@
-with import <nixpkgs> {};
-
-let nix = nixUnstable; in
-
-stdenv.mkDerivation {
-  name = "nix-serve-2";
-
-  buildInputs = [ perl nix perlPackages.Plack perlPackages.Starman perlPackages.DBDSQLite ];
-
-  unpackPhase = "true";
-
-  installPhase =
-    ''
-      mkdir -p $out/libexec/nix-serve
-      cp ${./nix-serve.psgi} $out/libexec/nix-serve/nix-serve.psgi
-
-      mkdir -p $out/bin
-      cat > $out/bin/nix-serve <<EOF
-      #! ${stdenv.shell}
-      PERL5LIB=$PERL5LIB exec ${perlPackages.Starman}/bin/starman $out/libexec/nix-serve/nix-serve.psgi "\$@"
-      EOF
-      chmod +x $out/bin/nix-serve
-    '';
-}
+(import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
+  src = ./.;
+}).defaultNix
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/nixos-20.09";
+
+  outputs = { self, nixpkgs }:
+
+    let
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+      systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "x86_64-darwin" ];
+    in {
+
+      overlay = final: prev: {
+
+        nix-serve = with final; stdenv.mkDerivation {
+          name = "nix-serve-${self.lastModifiedDate}";
+
+          buildInputs = [ perl nix.perl-bindings perlPackages.Plack perlPackages.Starman perlPackages.DBDSQLite ];
+
+          unpackPhase = "true";
+
+          installPhase =
+            ''
+              mkdir -p $out/libexec/nix-serve
+              cp ${./nix-serve.psgi} $out/libexec/nix-serve/nix-serve.psgi
+
+              mkdir -p $out/bin
+              cat > $out/bin/nix-serve <<EOF
+              #! ${stdenv.shell}
+              PERL5LIB=$PERL5LIB exec ${perlPackages.Starman}/bin/starman $out/libexec/nix-serve/nix-serve.psgi "\$@"
+              EOF
+              chmod +x $out/bin/nix-serve
+            '';
+        };
+
+      };
+
+      packages = forAllSystems (system: {
+        nix-serve = (import nixpkgs { inherit system; overlays = [ self.overlay ]; }).nix-serve;
+      });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.nix-serve);
+
+    };
+}

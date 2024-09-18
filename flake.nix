888b6bcb441596dd553e08914c25898d44c04818
Jörg Thalheim <joerg@thalheim.io>
2024-09-18 09:41:37 +0200

    make it possible to use nix-serve without overlay

--- flake.nix
+++ flake.nix
@@ -9,34 +9,15 @@
     in {
 
       overlay = final: prev: {
-
-        nix-serve = with final; stdenv.mkDerivation {
-          name = "nix-serve-${self.lastModifiedDate}";
-
-          buildInputs = [ perl nixVersions.latest.perl-bindings perlPackages.Plack perlPackages.Starman perlPackages.DBDSQLite ];
-
-          unpackPhase = "true";
-
-          installPhase =
-            ''
-              mkdir -p $out/libexec/nix-serve
-              cp ${./nix-serve.psgi} $out/libexec/nix-serve/nix-serve.psgi
-
-              mkdir -p $out/bin
-              cat > $out/bin/nix-serve <<EOF
-              #! ${stdenv.shell}
-              PERL5LIB=$PERL5LIB \
-              NIX_REMOTE="\''${NIX_REMOTE:-auto?path-info-cache-size=0}" \
-              exec ${perlPackages.Starman}/bin/starman --preload-app $out/libexec/nix-serve/nix-serve.psgi "\$@"
-              EOF
-              chmod +x $out/bin/nix-serve
-            '';
+        nix-serve = final.pkgs.callPackage ./package.nix {
+          inherit self;
         };
-
       };
 
       packages = forAllSystems (system: {
-        nix-serve = (import nixpkgs { inherit system; overlays = [ self.overlay ]; }).nix-serve;
+        nix-serve = nixpkgs.legacyPackages.${system}.callPackage ./package.nix {
+          inherit self;
+        };
       });
 
       defaultPackage = forAllSystems (system: self.packages.${system}.nix-serve);
